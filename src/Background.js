3c22a32dce515a90fa577e8f5bf0edfcbd7db32f
davidholyko <david.holy.ko@gmail.com>
2019-04-09 20:45:51 -0400

    add homepage to package lock

--- src/Background.js
+++ src/Background.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import BackgroundImage from './images/background-0.jpg'
+import BackgroundImage0 from './images/background-0.jpg'
+import BackgroundImage1 from './images/background-1.jpg'
+import BackgroundImage2 from './images/background-2.jpg'
+import BackgroundImage3 from './images/background-3.jpg'
+import BackgroundImage4 from './images/background-5.jpg'
 
 class Background extends Component {
   constructor () {
     super()
 
-    this.state = {}
+    this.state = {
+      currentImage: BackgroundImage0,
+      images:
+      [ BackgroundImage0,
+        BackgroundImage1,
+        BackgroundImage2,
+        BackgroundImage3,
+        BackgroundImage4 ]
+    }
+  }
+
+  componentDidMount () {
+    const chooseImage = () => this.setState({ currentImage: this.state.images[(Math.random() * 5) | 0] })
+    setInterval(chooseImage, 5000)
   }
 
   render () {
@@ -14,9 +31,10 @@ class Background extends Component {
       'maxHeight': '100vh',
       'overflowY': 'hidden'
     }
+
     return (
       <div className="w-100 h-100">
-        <img className="w-100 h-100" style={style} src={BackgroundImage}></img>
+        <img key={this.state.currentImage} className="w-100 h-100 fadeIn animated slow" style={style} src={this.state.currentImage}></img>
       </div>
     )
   }
--- src/Clock.js
+++ src/Clock.js
@@ -6,23 +6,44 @@ class Background extends Component {
     super()
 
     this.state = {
-      clock: ''
+      hoverClock: false,
+      clock: '',
+      date: ''
     }
   }
 
   componentDidMount () {
-    const updateClock = () => this.setState({ clock: moment().format('MMMM Do YYYY, h:mm:ss a') })
+    const updateDate = () => this.setState({ date: moment().format('MMMM Do YYYY') })
+    const updateClock = () => this.setState({ clock: moment().format('h:mm:ss a') })
     setInterval(updateClock, 500)
+    setInterval(updateDate, 500)
   }
 
+  hoverClock = () => this.setState({ hoverClock: !this.state.hoverClock })
+
   render () {
-    const style = {
-      'fontSize': '5rem'
+    const styleClock = {
+      'fontSize': '10rem',
+      'textShadow': '0 20px 20px black'
     }
 
+    const styleClockInverse = {
+      'fontSize': '10rem',
+      'textShadow': '0 20px 20px white',
+      'fontColor': 'black'
+    }
+
+    const styleDate = {
+      'fontSize': '2rem',
+      'textShadow': '0 20px 20px black'
+    }
+    const { date, clock, hoverClock } = this.state
+
     return (
-      <div className="fixed-top d-flex flex-column justify-content-center w-100 h-100">
-        <h1 className="d-inline-block text-light text-center" style={style}>{this.state.clock}</h1>
+
+      <div className="fixed-top d-flex flex-column w-100 h-100 justify-content-center">
+        <h1 className="fixed-top p-2 text-light ml-auto" style={styleDate}>{date}</h1>
+        <h1 className="d-inline-block text-light text-center" style={hoverClock ? styleClock : styleClockInverse} onMouseEnter={this.hoverClock}>{clock}</h1>
       </div>
     )
   }
